43464bd940213defa273721d9f8efcd4ee9367fe
kasturi8405-a11y <kasturi8405@gmail.com>
2026-03-02 12:03:13 +0800

    first commit

---
+++ py/01_variables_data_types.py
@@ -0,0 +1,31 @@
+
+name = "Kasturi" #String
+age = 42         #integer
+weight = 60      #Float
+is_female = True #Boolean
+her_name = "Alice"
+random_number = None   #NoneType
+tapir = "animal"
+
+print(name)
+print(type(age))
+print(her_name)
+print(type(random_number))
+
+x = 10
+y = 3
+
+print (x + y) #Addition
+print (x - y) #Substration
+print (x * y) #Multiplication
+print (x / y) #Division
+print (x // y) #Floor division
+print (x % y) #Modulus
+print (x ** y) #Exponentiation)
+
+C = celsius = 100
+F = celsius * 9/5 + 32
+print (C)
+print (F)
+print (C, "Celsius is" , F, "Fahrenheit")
+
